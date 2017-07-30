f69b0f255173b062033d8ca23d1695e222fad457
Martin Tóth <ezimir@gmail.com>
2017-07-30 22:26:23 +0200

    Make target table class name into a command argument
    Display default value in command help text.

--- tabletocsv.py
+++ tabletocsv.py
@@ -10,11 +10,13 @@ import sys
 from bs4 import BeautifulSoup
 
 
+DEFAULT_CLASS_NAME = 'wikitable'
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'url',
-        help = 'Source of data. Expects to contain one or more <table class="wikitable"> elements.'
+        help = 'Source of data. Expects to contain one or more <table> elements with specified class name.'
     )
     parser.add_argument(
         'dest',
@@ -23,13 +25,19 @@ def main():
         default = sys.stdout,
         help = 'The file to write output to. Omit or use \'-\' to write to stdout.'
     )
+    parser.add_argument(
+        '--class-name',
+        dest = 'class_name',
+        default = DEFAULT_CLASS_NAME,
+        help = 'CSS class name for the tables to search for. (Without dot, just name.) (default: %(default)s)'
+    )
     args = parser.parse_args()
 
     response = urllib2.urlopen(args.url)
     html = response.read()
     soup = BeautifulSoup(html, 'html.parser')
     data = []
-    for table in soup.findAll('table', {'class': 'wikitable'}):
+    for table in soup.findAll('table', {'class': args.class_name}):
         if not data:
             headers = table.findAll('th')
             data.append([h.text.encode('utf8') for h in headers])
